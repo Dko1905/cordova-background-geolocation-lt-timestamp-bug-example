2e59e32f55f69d516ae9f97d5d7ec175823b8c1f
Daniel Florescu <df@z5.lt>
2022-05-16 17:57:48 +0200

    Add example

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,27 @@
 const app = angular.module('app-template', ['onsen']);
 
 app.controller('PageController', function($scope) {
-	$scope.count = 0;
+	const geo = window.BackgroundGeolocation;
 
-	$scope.incrementCount = function() {
-		$scope.count += 1;
+	$scope.ready = function() {
+		geo.ready({
+			reset: true,
+			url: 'http://192.168.1.44',
+			autoSync: false,
+			batchSync: true
+		})
+			.then(_ => console.log('READY'))
+			.catch(err => console.error(Error(err)));
+	};
+
+	$scope.test = function() {
+		geo.resetOdometer() // <-- Bug right here
+			.then(location => {
+				console.log(JSON.stringify(location));
+				console.log(location.timestamp); // <-- Incorrect output
+				console.log(location.timestamp instanceof Date)
+				console.log(new Date() instanceof Date) // Just to be sure
+			})
+			.catch(err => console.error(Error(err)));
 	};
 });
